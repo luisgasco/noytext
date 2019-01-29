001611f2402f846e2593f0960ce37fb3737edb32
luisgasco <luisgascosanchez@gmail.com>
2019-01-29 18:25:00 +0100

    I have commented the config_files I have updated packrat I have changed the load_config_files.R file to work with the new commented config_files.

--- extra_code/load_config_files.R
+++ extra_code/load_config_files.R
@@ -10,34 +10,34 @@
 
 # HELP TEXTs CONFIGURATION FILES ------------------------
 help_text_path <- paste0(current_path,"/config_files/HelpTexts_conf.txt")
-help<-read.table(help_text_path, header = FALSE, sep = ":",col.names = c("NºHelp","Instruction"))[,2]
+help<-read.table(help_text_path, header = FALSE, sep = ":",col.names = c("NºHelp","Instruction"),blank.lines.skip=TRUE)[,2]
 
 # GENERAL CONFIGURATION FILES ------------------------
 gen_conf_text_path <- paste0(current_path,"/config_files/GeneralUI_conf.txt")
-gen_conf<-plyr::ldply(strsplit(readLines(gen_conf_text_path),":"),rbind)
-names(gen_conf) <- c("element","text_title","logical","file")
-gen_conf$element <- as.character(gen_conf$element)
-gen_conf$text_title <- as.character(gen_conf$text_title)
-gen_conf$logical <- as.character(gen_conf$logical)
-gen_conf$file <- as.character(gen_conf$file)
+gen_conf<- read.table(gen_conf_text_path,header=FALSE,blank.lines.skip = TRUE)
+gen_conf<- gen_conf %>% separate(V1,":",into=c("element","text_title","logical","file"),
+                                 extra = "merge")
+  
 
 # MONGODB DATABASE CONFIGURATION FILES ------------------
 mongo_conf_text_path <- paste0(current_path,"/config_files/MongoDB_conf.txt")
-mongo_conf<-read.table(mongo_conf_text_path, header = FALSE, sep = ":",col.names = c("parameter","value"),stringsAsFactors = FALSE)[,2]
+mongo_conf<-read.table(mongo_conf_text_path, header = FALSE, sep = ":",
+                       col.names = c("parameter","value"),stringsAsFactors = FALSE,
+                       blank.lines.skip=TRUE)[,2]
 
 
 # SURVEY CONF------------------
 survey_conf_text_path <- paste0(current_path,"/config_files/Survey_conf.txt")
-survey_needed <- as.logical(read.table(survey_conf_text_path,header=FALSE,sep=":", nrows=1)[,2])
-survey_num_questions <- as.numeric(read.table(survey_conf_text_path,header=FALSE,sep=":",skip = 1, nrows=1,stringsAsFactors = FALSE)[,2])
+survey_text <- read.table(survey_conf_text_path,header=FALSE,sep=":",stringsAsFactors = FALSE,
+                          blank.lines.skip=TRUE, fill = TRUE)
+names(survey_text) <-c("q_number","q_type","q_id","q_text","q_aditional")
 
-survey_questions <- read.table(survey_conf_text_path,header=FALSE,sep=":",skip = 2,
-                               nrows=survey_num_questions,stringsAsFactors = FALSE,
-                               col.names = c("q_number","q_type","q_id","q_text","q_aditional"))
+survey_needed <- as.logical(survey_text$q_type[1])
+survey_num_questions <- as.numeric(survey_text$q_type[2])
 
+
+survey_questions <- survey_text[c(3:(3+survey_num_questions-1)),]
 survey_questions$q_aditional <- strsplit(survey_questions$q_aditional,";")
 
-survey_mandatory <-strsplit(read.table(survey_conf_text_path,header=FALSE,sep=":",
-                                       skip=2+survey_num_questions,nrows=1,
-                                       stringsAsFactors = FALSE)[,2],
-                            ";")[[1]]
+# NOT IMPLEMENTED YET
+survey_mandatory <-strsplit(survey_text[c(2+survey_num_questions+1),"q_type"],";")[[1]]
